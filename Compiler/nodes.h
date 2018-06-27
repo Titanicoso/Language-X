0019e68e9c307a87fc001bd2d9f4f022eeb87ad9
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro-3.local>
2018-06-27 00:47:47 -0300

    Cambios - va en criollo porque el horario no esta para ingles - * nodos.h -> no son cambios significativos, son cambios en nombres de variables que no me gustaba por estilo que haya algunos que tengan elnombredelavariable_node, excepto en if, while, etc. que son palabras reservadas * nodos.c -> No recordaba si se podia asignar directamente NULL sin antes preguntar si dicha variable era o no NULL, por lo tanto preferí chequear y en caso de ser NULL directamente asignar NULL, en caso de no ser necesario, no hay drama, mañana lo cambio. - Incluí la función de error() donde entendí pertinente inclusive por ahi abusivo porque difícilmente haya un caso donde el enum no sea correcto, perrrro, defensivo ante todo diría mi amigo el leña, falta que la pensemos y hagamos - Idem con las funciones del chequeo de variables, estuve pensando que para hacernos la tarea mas fácil podríamos simplemente decir que las variables deben ser únicas .... al fin y al cabo tiene que ser distinto a C .... (buscando atajos) CREO QUE NO QUEDA MAS POR ACLARAR

--- Compiler/nodes.h
+++ Compiler/nodes.h
@@ -13,87 +13,88 @@ enum productions
 	typedef struct program_node{
 		struct defines_node * defines;
 		struct functions_node * functions;
-	} program_node;
+	} program_node; 
 
 	typedef struct defines_node{
 		struct define_node * define;
 		struct defines_node * next;
-	} defines_node;
+	} defines_node; 
 
 	typedef struct define_node{
 		enum productions production;
 		char* name;
 		char* string_name;
 		int value;
-	} define_node;
+	} define_node; 
 
 	typedef struct functions_node{
 		struct function_node * function;
 		struct functions_node * next;
-	} functions_node;
+	} functions_node; 
 
 	typedef struct function_node{
 		struct parameters_node * parameters;
 		struct sentences_node * sentences;
 		char * name;
-	} function_node;
+	} function_node; 
 
 	typedef struct parameters_node {
 		struct parameters_node * next;
 		char* name; /*NAME*/
-	} parameters_node;
+	} parameters_node; 
 
 	typedef struct sentences_node {
-		struct sentence_node * sentence_node;
-		struct sentences_node * sentences_node;
-	}sentences_node;
+		struct sentence_node * sentence;
+		struct sentences_node * sentences;
+	}sentences_node; 
 
 	typedef struct sentence_node{
 		enum productions production;
-		struct variable_opration_node *variable_opration_node;
+		struct variable_opration_node *variable_opration;
 		struct for_node *for_node;
 		struct while_node *while_node;
 		struct if_node *if_node;
-		struct function_execute_node* function_execute_node;
-		struct return_node*return_node;
+		struct function_execute_node *function_execute;
+		struct return_node *return_node;
 		char sentenceEnd; //Si es ; o nada.
 	}sentence_node;
 
 	typedef struct variable_opration_node{
 		enum productions production;
-		struct assignment_node *assignment_node;
+		struct assignment_node *assignment;
 		char * increment_decrement_name;
-	}variable_opration_node;
+	}variable_opration_node; 
 
 	typedef struct assignment_node{
 		enum productions production;
-		char* name;
-		char* string;
-		struct queue_stack_node * queue_stack;
-		char* assignment_operation;
-		struct expression_node *expression_node;
-	}assignment_node;
+		char *name;
+		char *string;
+		struct queue_stack_node *queue_stack;
+		char *assignment_operation;
+		struct expression_node *expression;
+	}assignment_node; 
 
 	typedef struct queue_stack_node{
-		struct elements_node * elements;
-	}queue_stack_node;
+		struct elements_node *elements;
+	}queue_stack_node; 
 
 	typedef struct elements_node{
-		struct element_node * element;
-		struct elements_node * next;
-	}elements_node;
+		struct element_node *element;
+		struct elements_node *next;
+	}elements_node; 
 
+	/*no es el nombre de la variable si no lo que se le asigna en si */
 	typedef struct element_node{
 		enum productions production;
-		char * string_name;
+		char *string_name;
 		int value;
-	}element_node;
+	}element_node;  
 
 	typedef struct if_node{
 		struct condition_node *condition;
 		struct sentences_node *sentences;
 		struct if_node *else_branch; /*en else pondrias condition en null & else en null, pero en if_else es lo mismo*/
-	}if_node;
+	}if_node; 
 
 	typedef struct while_node{
 		struct condition_node *condition;
@@ -102,53 +103,52 @@ enum productions
 
 	typedef struct for_node	{
 		enum productions production;
-		struct assignment_node *assignment_node;
-		struct condition_node *condition_node;
-		struct variable_operation_node *variable_operation_node;
+		struct assignment_node *assignment;
+		struct condition_node *condition;
+		struct variable_operation_node *variable_operation;
 		struct sentences_node *sentences;
 		char * variable;
 		char * structure;
-	}for_node;
+	}for_node; 
 
 	typedef struct condition_node{
 		enum productions production;
 		struct expression_node *expression_1;
 		char * logical_operation;
 		struct expression_node *expression_2;
-		struct condition_node *condition_node;
-	}condition_node;
+		struct condition_node *condition;
+	}condition_node; 
 
 	typedef struct expression_node{
 		enum productions production;
 		struct expression_node *expression_1;
 		char op;
 		struct expression_node *expression_2;
-		struct function_execute_node *function_execute_node;
-
+		struct function_execute_node *function_execute;
 		int boolean_number;
 		char * name;
-	}expression_node;
+	}expression_node; 
 
 	typedef struct function_execute_node{
 		char * name;
 		struct call_parameters_node * parameters;
-	}function_execute_node;
+	}function_execute_node; 
 
 	typedef struct call_parameters_node{
 		struct call_parameter_node * parameter;
 		struct call_parameters_node * next;
-	} call_parameters_node;
+	} call_parameters_node; 
 
 	typedef struct call_parameter_node{
 		enum productions production;
 		char* string;
-		struct expression_node *expression_node;
-	} call_parameter_node;
+		struct expression_node *expression;
+	} call_parameter_node; 
 
 	typedef struct return_node {
 		enum productions production;
 		char* string;
-		struct expression_node *expression_node;
-	} return_node;
+		struct expression_node *expression;
+	} return_node; 
 
 #endif
